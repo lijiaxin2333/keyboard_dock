7bdb5a02f8a7a96d62efad92f261a0ff162282b2
lijiaxin <2727191695@qq.com>
2026-01-10 11:18:07 +0800

    Initial Commit

---
+++ keybaord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keybaord
+//
+//  Created by jason li  on 2026/1/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keybaord/keybaordApp.swift
@@ -0,0 +1,17 @@
+//
+//  keybaordApp.swift
+//  keybaord
+//
+//  Created by jason li  on 2026/1/10.
+//
+
+import SwiftUI
+
+@main
+struct keybaordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
